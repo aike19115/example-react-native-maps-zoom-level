85366dd0cb3e1b8f40812d5c8f4c155fc3c75dea
Aike van den Brink <aikevandenbrink@gmail.com>
2023-07-14 22:59:29 +0200

    refactor: renamed MapsOnpress to MapsZoomLevel

--- android/app/src/main/java/com/examplereactnativemapszoomlevel/MainApplication.java
+++ android/app/src/main/java/com/examplereactnativemapszoomlevel/MainApplication.java
@@ -1,4 +1,4 @@
-package com.examplereactnativemapsonpress;
+package com.examplereactnativemapszoomlevel;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/ExampleReactNativeMapsZoomLevelTests/ExampleReactNativeMapsZoomLevelTests.m
+++ ios/ExampleReactNativeMapsZoomLevelTests/ExampleReactNativeMapsZoomLevelTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ExampleReactNativeMapsOnpressTests : XCTestCase
+@interface ExampleReactNativeMapsZoomLevelTests : XCTestCase
 
 @end
 
-@implementation ExampleReactNativeMapsOnpressTests
+@implementation ExampleReactNativeMapsZoomLevelTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
